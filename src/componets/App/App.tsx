fc023450c36f81fa2bf2b87eb0f89dc6508c4a70
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-27 16:55:50 +0600

    create app as component

---
+++ src/componets/App/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+import {FC} from "react";
+
+const App: FC = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default App;

8ab1a772c52d94195fbc68c64bb347325cd8f69e
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-27 16:56:50 +0600

    import styles and components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.scss'
+import App from "./componets/App/App";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
 

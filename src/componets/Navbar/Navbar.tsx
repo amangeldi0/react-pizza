111200339d91de81643848a6c2e2e4ed2fcb68f9
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-27 16:57:42 +0600

    layout navbar.tsx

---
+++ src/componets/Navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import {FC} from 'react';
+
+import logo from '../../assets/navabr-logo.svg'
+import cart from '../../assets/shoping-cart.svg'
+
+const Navbar: FC= () => {
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar__container">
+                    <div className="logo__block">
+                        <div className="logo"><img src={logo} alt="navbar__logo"/></div>
+                        <div className="title__block">
+                            <div className="title">REACT PIZZA</div>
+                            <div className="describe">самая вкусная пицца во вселенной</div>
+                        </div>
+                    </div>
+                    <div className="cart__block">
+                        <div className="price">520$</div>
+                        <hr/>
+                        <div className="cart">
+                            <img src={cart} alt="navbar__cart"/>
+                            <div className="cart__count">3</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr style={{background: '#F7F7F7'}}/>
+        </>
+    );
+};
+
+export default Navbar;

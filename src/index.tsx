479b734565dd045a4ff0b34a340796c9a0324ac0
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-29 11:12:23 +0600

    change style location

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.scss'
+import './style/index.scss'
 import App from "./components/App/App";
 
 const container = document.getElementById('root')!;

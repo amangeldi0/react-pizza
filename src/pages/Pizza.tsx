5d02d3469c9761654a4dfc1a8732cb1f4c719ae4
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-29 15:41:52 +0600

    import sort component

--- src/pages/Pizza.tsx
+++ src/pages/Pizza.tsx
@@ -2,6 +2,8 @@ import {FC, useEffect, useState} from 'react';
 import { IPizza } from "../types/types";
 import axios from "axios";
 import PizzaItem from "../components/PizzaItem/PizzaItem";
+import Sort from "../components/Sort/Sort";
+
 const Pizza: FC = () => {
     const [data, setData] = useState<IPizza[]>([]);
 
@@ -18,13 +20,16 @@ const Pizza: FC = () => {
         fetchPizza()
     }, [])
     return (
-        <div className="pizza__page">
-            <div className="pizza__page__container">
-                {data.map(item => {
-                    return <PizzaItem key={item.id} props={item}/>
-                })}
-            </div>
-        </div>
+       <>
+           <Sort />
+           <div className="pizza__page">
+               <div className="pizza__page__container">
+                   {data.map(item => {
+                       return <PizzaItem key={item.id} props={item}/>
+                   })}
+               </div>
+           </div>
+       </>
     );
 };
 

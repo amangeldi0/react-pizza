3f328bd3974264987daf4a9f9ab85ce1447eb656
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-31 22:39:37 +0600

    add provider store

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style/index.scss'
+import { Provider} from "react-redux";
 import App from "./components/App/App";
-
+import store from "./redux/store";
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>
 );
 

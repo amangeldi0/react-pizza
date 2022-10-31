ac5a6c8b36996453ef339261236fbf989084df1f
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-31 22:43:11 +0600

    fetch types

---
+++ src/redux/data/type.ts
@@ -0,0 +1,10 @@
+export type Pizza = {
+    id: string;
+    title: string;
+    price: number;
+    image: string;
+    sizes: string[];
+    types: string[];
+    category: string[];
+    rate: number
+};

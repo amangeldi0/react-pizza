d8e5519db0ae024a0f4d55b7a306bf4372ee6278
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-01 17:39:01 +0600

    change variable name

--- src/redux/data/selector.ts
+++ src/redux/data/selector.ts
@@ -1,19 +1,20 @@
 import { RootState } from '../store';
 import {createSelector} from "@reduxjs/toolkit";
-import {activeSort} from "../filter/selectors";
+import {activeFilter, activeSort} from "../filter/selectors";
 export const pizzaDates = (state: RootState) => state.pizza.items
 export const filterPizza = createSelector(
     pizzaDates,
     activeSort,
-    (pizzas, filter) => {
-        if (filter === 'все'){
+    (pizzas, category) => {
+        if (category === 'все'){
             return pizzas
         }else{
-            if (filter === 'все'){
+            if (category === 'все'){
                 return pizzas
             }else{
-                return pizzas.filter(item => item.category.includes(filter))
+                return pizzas.filter(item => item.category.includes(category))
             }
         }
     }
 )
+

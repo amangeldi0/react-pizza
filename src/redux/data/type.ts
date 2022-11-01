9ba89fa7a82f0c9db88ed6efe6f5895b2a522c2d
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-01 09:46:07 +0600

    PizzaSlice types

--- src/redux/data/type.ts
+++ src/redux/data/type.ts
@@ -3,8 +3,19 @@ export type Pizza = {
     title: string;
     price: number;
     image: string;
-    sizes: string[];
+    size: string[];
     types: string[];
     category: string[];
     rate: number
 };
+
+export interface PizzaSliceState {
+    items: Pizza[];
+    status: Status;
+}
+
+export enum Status {
+    LOADING = 'loading',
+    SUCCESS = 'idle',
+    ERROR = 'error',
+}

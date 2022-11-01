ab972785835164802b69123204b991656d0dd61f
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-01 09:43:48 +0600

    add filterFunc in selector.ts

--- src/redux/data/selector.ts
+++ src/redux/data/selector.ts
@@ -1,3 +1,19 @@
 import { RootState } from '../store';
-import {pizzaAdapter} from "./PizzaSlice";
-export const {selectAll} = pizzaAdapter.getSelectors((state: RootState) => state.pizza)
+import {createSelector} from "@reduxjs/toolkit";
+import {activeSort} from "../filter/selectors";
+export const pizzaDates = (state: RootState) => state.pizza.items
+export const filterPizza = createSelector(
+    pizzaDates,
+    activeSort,
+    (pizzas, filter) => {
+        if (filter === 'все'){
+            return pizzas
+        }else{
+            if (filter === 'все'){
+                return pizzas
+            }else{
+                return pizzas.filter(item => item.category.includes(filter))
+            }
+        }
+    }
+)

897c025c8dd977d7c320e5cf085cb0428f7d7666
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-01 23:18:12 +0600

    add new button for search

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,13 @@ import {FC} from 'react';
 import {Link} from "react-router-dom";
 import logo from '../../assets/navabr-logo.svg'
 import cart from '../../assets/shoping-cart.svg'
+import searchIcon from "../../assets/search.svg";
+
+import {activeInputChanged} from '../../redux/filter/filterSlice'
+import Search from "../Search/Search";
+import {useAppDispatch} from "../../redux/store";
 const Navbar: FC= () => {
+    const dispatch = useAppDispatch()
     return (
         <>
             <div className="navbar">
@@ -14,10 +20,19 @@ const Navbar: FC= () => {
                             <div className="describe">самая вкусная пицца во вселенной</div>
                         </div>
                     </div>
-                    <Link to={'/cart'} className="cart__block">
-                        Cart
-                        <img src={cart} alt=""/>
-                    </Link>
+                    <Search />
+                    <div className='items__block'>
+                        <div className='search__button'>
+                            <img
+                                src={searchIcon}
+                                alt="searchIcon"
+                                onClick={() => dispatch(activeInputChanged(true))}/>
+                        </div>
+                        <Link to={'/cart'} className="cart__block">
+                            Cart
+                            <img src={cart} alt=""/>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>

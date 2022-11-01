9880ddba5e024f30d17bef49d04426abd0562b99
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-01 23:19:14 +0600

    add search func in selector

--- src/redux/data/selector.ts
+++ src/redux/data/selector.ts
@@ -1,18 +1,19 @@
 import { RootState } from '../store';
 import {createSelector} from "@reduxjs/toolkit";
-import {activeFilter, activeSort} from "../filter/selectors";
+import {activeSearch, activeSort} from "../filter/selectors";
 export const pizzaDates = (state: RootState) => state.pizza.items
 export const filterPizza = createSelector(
     pizzaDates,
     activeSort,
-    (pizzas, category) => {
+    activeSearch,
+    (pizzas, category, search) => {
         if (category === 'все'){
-            return pizzas
+            return pizzas.filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
         }else{
             if (category === 'все'){
-                return pizzas
+                return pizzas.filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
             }else{
-                return pizzas.filter(item => item.category.includes(category))
+                return pizzas.filter(item => item.category.includes(category)).filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
             }
         }
     }

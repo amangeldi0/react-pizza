c3cb1b332c51999b84f832682a03297b2dfdeabf
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-01 23:20:14 +0600

    erase active filter export and export new state

--- src/redux/filter/selectors.ts
+++ src/redux/filter/selectors.ts
@@ -1,4 +1,4 @@
 import { RootState } from '../store';
-export const activeFilter = (state: RootState) => state.filter.activeFilter;
 export const activeSort = (state: RootState) => state.filter.activeSort;
 export const activeSearch = (state: RootState) => state.filter.activeSearch;
+export const activeInput = (state: RootState) => state.filter.activeInput;

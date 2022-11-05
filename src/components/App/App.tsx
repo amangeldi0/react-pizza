9bdac934db323ea4932ea9329063347ad2e0b7c8
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-05 12:28:01 +0600

    add lazy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
-import {FC} from "react";
+import {FC, lazy} from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
-import Pizza from "../../pages/Pizza";
 import Cart from "../../pages/Cart";
 import NotFound from "../../pages/NotFound";
 import TakeOrder from "../TakeOrder/TakeOrder";
+
+const Pizza = lazy(() => import("../../pages/Pizza"))
 
 const App: FC = () => {
     return (
